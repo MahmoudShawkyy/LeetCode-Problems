00bbb818548abe863ff5c8ec4a81d2660b6a890c
Mahmoud Shawky <mahmoudshawky2250@gmail.com>
2022-09-14 19:20:42 +0200

    Time: 77 ms (31.46%), Space: 14 MB (96.16%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        stack=[]
+        for c in ops:
+            if c=="D":
+                stack.append(stack[-1]*2)
+            elif c =="C":
+                stack.pop()
+            elif c=="+":
+                stack.append(stack[-1]+stack[-2])
+            else:
+                stack.append(int(c))
+        return sum(stack)
